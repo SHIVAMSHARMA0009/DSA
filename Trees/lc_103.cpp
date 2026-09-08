b610f465cd47d704e429ad547a06978567ecc2bb
Shivam Sharma <shivamsharma.se09@gmail.com>
2026-09-08 21:56:33 +0530

    zig-zag traversal

--- Trees/lc_103.cpp
+++ Trees/lc_103.cpp
@@ -1,4 +1,4 @@
-/* we are given binary tree , return its zig-zag traversal */
+/* we are given binary tree , return its zig-zag traversal    || ZIGZAG : L->R then R->L then L->R ...... */  
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -38,29 +38,31 @@ vector<vector<int>> zigzag(Node* root) {
     if(root == NULL) return ans;
 
     int LtoRdir = true;
-    queue<Node*> q;
+    queue<Node*> q;                // push root into queue
     q.push(root);
 
     while(!q.empty()) {
 
-        int n = q.size();
-        vector<int>temp(n);
+        int n = q.size();       
+        vector<int>temp(n);          // taking vector size equal to no. of child of root that is already pushed to queue
 
         for(int i=0;i<n;i++) {
             Node* front = q.front();
             q.pop();
 
-            int index = LtoRdir ? i : n - i - 1;
+            int index = LtoRdir ? i : n - i - 1;          // if L -> R start with 0 , if R -> L then from index of vector
             temp[index] = front->data;
 
-            if(front->left) q.push(front->left);
+            if(front->left) q.push(front->left);          // push the children of current node into queue
             if(front->right) q.push(front->right);
         }
-        LtoRdir = !LtoRdir;
+
+        LtoRdir = !LtoRdir;      // after completing one level change the direction for next level
         ans.push_back(temp);
     }
     return ans;
 }
+
 int main() {
 
     cout<<"Enter Tree Data : ";
